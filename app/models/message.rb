03c5fbd459a34662adbb36ad835cd5a13f95001c
TSL Studnet Mac <>
2012-11-30 17:22:03 -0600

    added geocode numbers

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,14 @@
 class Message < ActiveRecord::Base
   attr_accessible :copy, :latitude, :longitude, :user_id
   
-  # geocoded_by :latitude, :longitude
-  # after_validation :geocode
+  geocoded_by :ip_address,
+    :latitude => :lat, :longitude => :lon
+  after_validation :geocode
   
+  belongs_to :user
+  
+  def getGeo
+    @geo = Geocoder.search(request.ip)
+  end
    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :username
+  
+  has_many :messages
 end
